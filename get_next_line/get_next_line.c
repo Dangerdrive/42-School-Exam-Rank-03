9bf592dea8eb973ec4a018997149d284188aa52d
Felippe de Alencar <124204685+Dangerdrive@users.noreply.github.com>
2024-09-07 19:04:01 -0300

    Update get_next_line.c

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -3,15 +3,17 @@
 char	*ft_strdup(char *src)
 {
 	char	*dest;
-	int		i;
+	int i = 0;
 
-	i = 0;
 	while (src[i])
 		i++;
 	dest = (char *)malloc(sizeof(char) * (i + 1));
-	i = -1;
-	while (src[++i])
+	i = 0;
+	while (src[i])
+	{
 		dest[i] = src[i];
+		i++;
+	}
 	dest[i] = '\0';
 	return (dest);
 }
@@ -22,9 +24,8 @@ char	*get_next_line(int fd)
 	char		line[70000];
 	static int	buffer_readed;
 	static int 	buffer_pos;
-	int			i;
+	int	i = 0;
 
-	i = 0;
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	while (1)
